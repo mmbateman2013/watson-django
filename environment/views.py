3ed4698c287c9ab776984d5711132c9f06039b82
Mark Berneti <markb50k@gmail.com>
2017-06-16 17:01:02 +0000

    integrated Google contacts

--- environment/views.py
+++ environment/views.py
@@ -1,10 +1,54 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-from .models import Environment, Collection
+from .models import Environment, Collection, Google_Contact
 from .forms import QueryForm
 from . import services
 
+def contacts(request):
+    contact_list = services.get_contacts()
+    #create new Contacts
+    for person in contact_list:
+        #first see if Contact already exists
+        crn = person.get('resourceName')
+        
+        #displayname
+        names = person.get('names', [])
+        if len(names) > 0:
+            name = names[0].get('displayName')
+        else:
+            name = "No Display Name"
+            
+        #emailaddress
+        emailaddresses = person.get('emailAddresses', [])
+        if len(emailaddresses) > 0:
+            email_address = emailaddresses[0].get('value')
+        else:
+            email_address = ""
+            
+        #phonenumber
+        phonenumbers = person.get('phoneNumbers', [])
+        if len(phonenumbers) > 0:
+            phone_no = phonenumbers[0].get('value')
+        else:
+            phone_no = ""
+        
+
+        try:
+            gc = Google_Contact.objects.get(contact_resource_name=crn)
+        except Google_Contact.DoesNotExist:
+            gc = Google_Contact(contact_resource_name=crn, contact_name=name)
+            
+        gc.contact_name = name
+        gc.contact_email = email_address
+        gc.contact_phone_no = phone_no
+        gc.save()
+    
+    alpha_contact_list = Google_Contact.objects.order_by('contact_name')
+    context = {
+        'alpha_contact_list': alpha_contact_list,
+    }
+    return render(request, 'environment/contacts.html', context)
 
 def query(request, environment_id):
     if request.method == 'POST':
